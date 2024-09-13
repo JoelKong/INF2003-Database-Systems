db6c076d75a207fdb16ef6026e8017ffe492a784
Joel Kong <joelkong2001@gmail.com>
2024-09-13 13:38:20 +0800

    fdsfsd

--- client/src/pages/listofpets/ListOfPets.tsx
+++ client/src/pages/listofpets/ListOfPets.tsx
@@ -1,9 +1,11 @@
 import NavBar from "../general/NavBar";
+import Pets from "./components/Pets";
 
 export default function ListOfPets(): JSX.Element {
   return (
-    <>
+    <div className="h-screen w-screen">
       <NavBar />
-    </>
+      <Pets />
+    </div>
   );
 }
---
+++ client/src/pages/listofpets/components/Pets.tsx
@@ -0,0 +1,9 @@
+export default function Pets(): JSX.Element {
+  return (
+    <section className="w-screen h-screen flex justify-center items-center text-gray-700">
+      <div className="w-11/12 border-2 h-4/5 bg-white rounded-lg flex flex-col items-center p-4">
+        <h2 className="font-bold text-2xl">List Of Pets</h2>
+      </div>
+    </section>
+  );
+}
